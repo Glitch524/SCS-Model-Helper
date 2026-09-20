2a39d2ce2735a9b7a881a7f782827fefbad48ae7
Glitch <JohnLloyd524@hotmail.com>
2026-09-21 01:43:32 +0800

    Add Test button

--- Main/NaviMainWindow.xaml.cs
+++ Main/NaviMainWindow.xaml.cs
@@ -3,7 +3,9 @@ using SCS_Mod_Helper.ConverterPix;
 using SCS_Mod_Helper.Localization;
 using SCS_Mod_Helper.Modding.PaintJob;
 using SCS_Mod_Helper.Setting;
+using SCS_Mod_Helper.Trucks;
 using SCS_Mod_Helper.Utils;
+using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using Wpf.Ui.Input;
@@ -16,7 +18,7 @@ public partial class NaviMainWindow: BaseWindow {
 	readonly MainWindowBinding binding;
 	public NaviMainWindow() {
 		InitializeComponent();
-		binding = new(new(OpenWindow));
+		binding = new(new(OpenWindow), new(Test));
 		GridMain.DataContext = binding;
 	}
 
@@ -61,11 +63,19 @@ public partial class NaviMainWindow: BaseWindow {
 		window.ShowDialog();
 	}
 
+	private void Test(string? value) {
+		Debug.WriteLine(Paths.SiiFile("project", "id", "type", "name"));
+	}
+
 	private void NaviMainWindowLoaded(object sender, RoutedEventArgs e) => Navigation.Navigate("ModManifest");
 
 }
 
-public class MainWindowBinding(RelayCommand<string> windowCommand): BaseBinding {
+public class MainWindowBinding(RelayCommand<string> windowCommand, RelayCommand<string> testCommand): BaseBinding {
 
 	public RelayCommand<string> WindowCommand { get; } = windowCommand;
+
+	public RelayCommand<string> TestCommand { get; } = testCommand;
+
+	public Visibility TestVisibility => Debugger.IsAttached ? Visibility.Visible : Visibility.Collapsed;
 }
